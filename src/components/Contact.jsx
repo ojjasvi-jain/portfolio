2dd83f53ce7b39abd2baa39b2112618e180cc550
ojjasvi-jain <ojjasvi201@gmail.com>
2023-08-01 21:52:13 +0530

    initial commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,39 +1,79 @@
-import React from 'react'
+import React from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const form = React.useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_tav1lkj",
+        "template_tjpckkp",
+        form.current,
+        "BWupZll2bC57YZzXk"
+      )
+      .then(
+        (result) => {
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <div name='contact' className='w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white'>
-        <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
-                    Contact
-                </p>
-                <p className='py-6'>Submit the form below to get in touch with me</p>
-            </div>
-            <div className='flex justify-center items-center'>
-                <form action="https://getform.io/f/a4a95d59-4e17-4b65-9f09-215a996b1d68" method='POST' className='flex flex-col w-full md:w-1/2'>
-                    <input 
-                        type="text" 
-                        name='name' 
-                        placeholder='Enter your name' 
-                        className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
-                    <input 
-                        type="text" 
-                        name='email' 
-                        placeholder='Enter your email' 
-                        className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
-                    <textarea 
-                        name="message" 
-                        placeholder='Enter your message'
-                        rows="10" 
-                        className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'></textarea>
-
-                    <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-105 duration-300'>Let's talk</button>
-                </form>
-            </div>
+    <div
+      name="contact"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Contact
+          </p>
+          <p className="py-6">Submit the form below to get in touch with me</p>
         </div>
-    </div>
-  )
-}
+        <div className="flex justify-center items-center">
+          <form
+            action=""
+            method="POST"
+            ref={form}
+            onSubmit={sendEmail}
+            className="flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              id="to_name"
+              name="to_name"
+              placeholder="Enter your name"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              type="text"
+              id="user_email"
+              name="user_email"
+              placeholder="Enter your email"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Enter your message"
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
 
-export default Contact
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-105 duration-300">
+              Let's talk
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,68 +1,75 @@
-import React from 'react'
-import {FaGithub, FaLinkedin} from 'react-icons/fa'
-import {HiOutlineMail} from 'react-icons/hi'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 const SocialLinks = () => {
-
-  const links=[
+  const links = [
     {
       id: 1,
       child: (
         <>
-          LinkedIn<FaLinkedin size={30}/>
+          LinkedIn
+          <FaLinkedin size={30} />
         </>
       ),
-      href: 'https://linkedin.com',
-      style:'rounded-tr-md'
+      href: "https://in.linkedin.com/in/ojjasvi-jain-882650213",
+      style: "rounded-tr-md",
     },
     {
       id: 2,
       child: (
         <>
-          GitHub<FaGithub size={30}/>
+          GitHub
+          <FaGithub size={30} />
         </>
       ),
-      href: 'https://github.com',
-      
+      href: "https://github.com/ojjasvi-jain",
     },
     {
       id: 3,
       child: (
         <>
-          Mail<HiOutlineMail size={30}/>
+          Mail
+          <HiOutlineMail size={30} />
         </>
       ),
-      href: 'mailto:trishitanandy7@gmail.com',
-      
+      href: "mailto:ojjasvi102@gmail.com",
     },
     {
       id: 4,
       child: (
         <>
-          Resume<BsFillPersonLinesFill size={30}/>
+          Resume
+          <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: 'https://drive.google.com/file/d/1AY8Y_TxP02PSc7bfLRTAtlqr-f1NtAPN/view?usp=drive_link',
-      
+      href: "https://drive.google.com/file/d/1T76b2RJfm4X4m49cjPs1fkr1w1pLGIWb/view",
     },
-  ]
+  ];
 
   return (
-    <div className='hidden  lg:flex flex-col top-[35%] left-0 fixed'>
-        <ul>
-
-          {links.map(({id,child,href,style})=>(
-              <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500'+" "+style}>
-              <a href={href} className='flex justify-between items-center w-full text-white'>
-                {child}
-              </a>
-            </li>
-          ))}
-
-          
-        </ul>
+    <div className="hidden  lg:flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        {links.map(({ id, child, href, style }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
+              " " +
+              style
+            }
+          >
+            <a
+              href={href}
+              className="flex justify-between items-center w-full text-white"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default SocialLinks
+export default SocialLinks;
